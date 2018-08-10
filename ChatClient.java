eec509229cea2745297f313e42d3457a47b0a3d9
Cody Lewis <luxdotsugi@gmail.com>
2018-08-10 13:29:37 +1000

    Added initial class files

---
+++ ChatClient.java
@@ -0,0 +1,10 @@
+/**
+ * ChatClient.java - SENG3400A1
+ * A socket based half duplex chat client
+ *
+ * @author Cody Lewis
+ * @since 2018-08-10
+ */
+public class ChatClient {
+
+}
---
+++ ChatServer.java
@@ -0,0 +1,10 @@
+/**
+ * ChatServer.java - Seng3400A1
+ * A socket based half duplex chat server
+ *
+ * @author Cody Lewis
+ * @since 2018-08-10
+ */
+public class ChatServer {
+
+}
